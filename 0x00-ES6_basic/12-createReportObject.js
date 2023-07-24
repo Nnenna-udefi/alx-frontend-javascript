069c6728ce1c7dee4625d017ae6dd0a0666288a5
Nnenna Udefi <miriamudefi@gmail.com>
2023-07-24 20:07:57 +0100

    eslint

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+
+export default function createReportObject(employeesList) {
+    const allEmployees = {...employeesList, };
+    
+    const reportObject = {
+        allEmployees,
+        getNumberOfDepartments(employeesList) {
+          return Object.keys(employeesList).length;
+        },
+      };
+    
+      return reportObject;
+}
