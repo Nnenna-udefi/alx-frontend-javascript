44fc0164b90fc4621175fb1c183bb742ef6d5202
Nnenna udefi <miriamudefi@gmail.com>
2023-07-26 20:48:17 +0000

    error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,26 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-  return Promise.allSettled([userPromise, photoPromise]);
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+    const [user, photo] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      {
+        status: user.status,
+        value: user.status === 'fulfilled' ? user.value : user.reason,
+      },
+      {
+        status: photo.status,
+        value: photo.status === 'fulfilled' ? photo.value : photo.reason,
+      },
+    ];
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
 }
 
 export default handleProfileSignup;
