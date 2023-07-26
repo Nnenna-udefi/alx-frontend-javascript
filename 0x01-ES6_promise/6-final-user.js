80935cd9839c1eb7567af89d3bbd83cd1bcb3e94
Nnenna udefi <miriamudefi@gmail.com>
2023-07-26 21:10:18 +0000

    added error message to try function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
+  firstName, lastName, fileName,
 ) {
   const res = [];
   try {
