0c8e2cdc47f15695458940f95bb4c4b5cec98247
Nnenna udefi <miriamudefi@gmail.com>
2023-08-02 13:11:46 +0000

    condition statement

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,16 @@ const weakMap = new WeakMap();
 export { weakMap };
 
 export const queryAPI = (endpoint) => {
-  let queryCount = weakMap.get(endpoint) || 0;
-  if (queryCount >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+  if (weakMap.has(endpoint)) {
+    let queryCount = weakMap.get(endpoint) || 0;
+    if (queryCount >= 5) {
+      throw new Error('Endpoint load is high');
+    }
 
-  // Increment the query count for the endpoint in the weakMap
-  queryCount += 1;
-  weakMap.set(endpoint, queryCount);
+    // Increment the query count for the endpoint in the weakMap
+    queryCount += 1;
+    weakMap.set(endpoint, queryCount);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
 };
